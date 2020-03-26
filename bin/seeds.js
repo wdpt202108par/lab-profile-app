d76d90225aebe8aeba6037aeedc9ee68ea03cdb6
abernier <antoine.bernier@gmail.com>
2020-03-26 16:28:52 +0100

    bcryptjs + update modules

--- bin/seeds.js
+++ bin/seeds.js
@@ -8,7 +8,7 @@ require('dotenv').config({
 });
 
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 const bcryptSalt = 10;
